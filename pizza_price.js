6e81d7369c38874539bfb23fb2f13218afbe76f7
ME <mic.a.eaton@gmail.com>
2022-07-21 08:25:53 -0400

    added best price highlight

--- pizza_price.js
+++ pizza_price.js
@@ -2,6 +2,8 @@
 let pieCount		= 0;
 let daemonActive	= false;
 
+
+
 function newPie(){
 	//creates a NewPie box
 	++pieCount;
@@ -14,7 +16,6 @@ function newPie(){
 	//data Entry Area
 	newPieElement.innerHTML += "<table><tr><td>Diameter(inches)</td><td><input type=\"number\" step=\"1\" class=\"input\" id=\"size" + newDivName + "\"></td></tr><tr><td>Cost($)</td><td><input type=\"number\" step=\"0.01\" class=\"input\" id=\"price" + newDivName + "\"></td></tr></table>"
 
-
 	//result area
 	newPieElement.innerHTML += "<div id=\"result" + newDivName + "\">  </div>  "
 
@@ -24,17 +25,18 @@ function newPie(){
 	//check to see if calculate daemon already created
 	if(daemonActive){
 		//create daemon
-
 	} 
 	
+}//end newPie()
 
-}//end drawBlueBox()
+
 
 function calculatePrices(){
 	//get a list of all pies in mainArea div
 	//this will be all children in mainArea div with the class pizzaPie
-	let allPies = document.getElementsByClassName("pizzaPie");
-	
+	let allPies   = document.getElementsByClassName("pizzaPie");
+	let allPrices = [];
+
 	if(allPies.length !=0){
 		for(let i =0; i < allPies.length; i++){
 		//iterate over list of these pie divs
@@ -45,12 +47,52 @@ function calculatePrices(){
 			let price   = Number(document.getElementById("price" + currentPie).value);
 			let radius  = Number(document.getElementById("size"  + currentPie).value) / 2;
 			
-			//calculate price/unit size
-			let pieArea    = (3.14159) * radius * radius;
-			let costResult = price / pieArea;
+			//is valid values?
+			if((price > 0 && !isNaN(price)) || (radius > 0 && !isNaN(radius))){
+				//calculate price/unit size
+				let pieArea    = (3.14159) * radius * radius;
+				let costResult = price / pieArea;
 
-			//write this calculated value to output element
-			document.getElementById("result" + currentPie).innerHTML = "Price/Sq.In. = $" + String(costResult.toFixed(2));
+				//write this calculated value to output element
+				document.getElementById("result" + currentPie).innerHTML = "Price/Sq.In. = $" + String(costResult.toFixed(2));
+				allPrices[i] = costResult;
+			} else {
+				document.getElementById("result" + currentPie).innerHTML = "Price/Sq.In. = $ 0.00";
+				allPrices[i] = 0;
+			}
 		}
 	}
-}
+
+	//find best price
+	highlightLowestPrice(allPies, allPrices);
+}//end calculatePrices()
+
+
+
+function highlightLowestPrice(pies, prices){
+	//variables
+	let currentLowest 	= 0;
+	
+	//find current lowest price 
+	for(let i = 0;i<pies.length; i++){
+		if(prices[i] == 0){
+			//is this number 0?
+			//do nothing
+		} else if(currentLowest == 0 || prices[i] < currentLowest){
+			currentLowest = prices[i];	
+		} 
+	}
+
+	//change highlights
+	if(currentLowest == 0){
+		return 0;
+	}
+	for(let i =0; i<pies.length; i++){
+		if(prices[i] == currentLowest){
+			pies[i].classList.add("bestPricePie")
+		}else if(pies[i].classList.contains("bestPricePie")){
+			pies[i].classList.remove("bestPricePie")
+		}
+	}
+
+}//end highlightLowestPrice()
